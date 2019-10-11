4e142ac079e4fd500866306e16ce2f9a6e0e5b08
Grievous Angel <ronfud2000@gmail.com>
2019-10-10 23:48:59 -0400

    reworked portfolio

---
+++ assets/javascript/portfolio.js
@@ -0,0 +1,22 @@
+    // changeColor alternates background image and text color for banner
+    // uses cnt to simplify testing condition
+    var cnt = 1;
+    function changeColor() {
+        // if cnt === 1 change initial css image
+            if (cnt === 1) {
+                $("#banner").text("Full Stack Web Developer");
+                $("#banner").css("color", "gold");
+                $("#banner").css("background-color", "red");
+                cnt = 2;
+            }
+            else {
+                // change back to original image
+                $("#banner").text("Ron Johnson");
+                $("#banner").css("color", "black");
+                $("#banner").css("background-color", "yellow");
+                cnt = 1;
+            }
+        }
+        // start changeColor
+        setInterval(changeColor, 3000);
+    
